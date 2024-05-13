a88dd505c38ddbe325d571323837f844b71f2f8d
FCPongs <metherelax@gmail.com>
2024-05-14 00:16:38 +0800

    Application page created, routing done

---
+++ frontend/FurFinder/src/Pages/Application.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import ApplicationForm from '../Components/ApplicationForm'
+
+const Application = () => {
+  return (
+  <>
+  <NavBar/>
+  <ApplicationForm/>
+  </>
+  )
+}
+
+export default Application
