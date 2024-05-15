0e10a1d0b15358853598a8a617fb8dbd0be1d2f1
Rhaey <rhaeyislyfe@gmail.com>
2024-05-15 14:33:35 +0800

    feat: add adopted_by

--- backend/pets/models.py
+++ backend/pets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 class Shelter(models.Model):
     name = models.CharField(max_length=200)
@@ -57,6 +58,7 @@ class Pet(models.Model):
     # relationships 
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE)
     breed = models.ForeignKey(Breed, on_delete=models.DO_NOTHING)
+    adopted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="Adopted By", null=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
